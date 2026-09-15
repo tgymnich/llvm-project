e0a102366a4c07d9e4d2ab262ce1a1c85fa11cb1
Ron Lieberman <ron.lieberman@amd.com>
2026-09-14 21:08:56 -0500

    Revert "[AMDGPU] Add `getLocalMemorySize` and `getAddressableLocalMemorySize` to TargetParser (#220718)"
    might break rocfft hipfft
    
    This reverts commit 3b8919bc85f33419acd3ac096a4265db3f41a32f.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.cpp
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.cpp
@@ -181,10 +181,9 @@ GCNSubtarget &GCNSubtarget::initializeSubtargetDependencies(const Triple &TT,
   if (FlatOffsetBitWidth == 0)
     FlatOffsetBitWidth = 13;
 
-  LocalMemorySize =
-      AMDGPU::getLocalMemorySize(getTargetID().getGPUKind(), isFullSIMDMode());
-  AddressableLocalMemorySize = AMDGPU::getAddressableLocalMemorySize(
-      getTargetID().getGPUKind(), isFullSIMDMode());
+  LocalMemorySize = AMDGPU::IsaInfo::getLocalMemorySize(*this);
+  AddressableLocalMemorySize =
+      AMDGPU::IsaInfo::getAddressableLocalMemorySize(*this);
   // LDS Allocation Granularity calculated in bytes from dwords
   LDSAllocationGranularity =
       AMDGPU::getLdsDwGranularity(*this) * sizeof(uint32_t);
@@ -250,7 +249,8 @@ GCNSubtarget::GCNSubtarget(const Triple &TT, StringRef GPU, StringRef FS,
   LLVM_DEBUG(dbgs() << "sramecc setting for subtarget: "
                     << TargetID.getSramEccSetting() << '\n');
 
-  NumWorkGroupSIMDs = AMDGPU::getNumWorkGroupSIMDs(isFullSIMDMode());
+  NumWorkGroupSIMDs =
+      AMDGPU::getNumWorkGroupSIMDs(AMDGPU::isFullSIMDMode(*this));
 
   TSInfo = std::make_unique<AMDGPUSelectionDAGInfo>();
 
