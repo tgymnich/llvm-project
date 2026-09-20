1548561488f9c5c508ef5159405ca52fe7c903df
z1-cciauto <z1_cciauto@amd.com>
2026-09-20 02:15:27 -0500

    Update LLVM_MAIN_REVISION to 597967 (#4544)
    Automated update of `LLVM_MAIN_REVISION` to match llvm/llvm-project
    `main` as merged into ROCm/amd-staging history.
    
    Computed revision: **597967**

--- llvm/include/llvm/Config/llvm-config.h.cmake
+++ llvm/include/llvm/Config/llvm-config.h.cmake
@@ -17,7 +17,7 @@
 /* The number of commits in the linear history from the
  * start of the universe up to the latest llvm main commit
  * that has been merged */
-#define LLVM_MAIN_REVISION 597915
+#define LLVM_MAIN_REVISION 597967
 
 /* Define if LLVM_ENABLE_DUMP is enabled */
 #cmakedefine LLVM_ENABLE_DUMP
